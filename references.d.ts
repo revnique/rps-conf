ce55c226a0aeac29e1639e6c58c5e9017060e443
Dominique L. Harris <dharris@harristeq.local>
2017-03-30 22:29:29 -0500

    initial commit

---
+++ references.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es6.d.ts" /> Needed for autocompletion and compilation.
+/// <reference path='./node_modules/typescript/lib/lib.es2016.d.ts'/>
+declare type FormData = any;
+declare var FormData;
