20307b0aa6d98121ef9824948ee3f6959d61ecf4
Dominique L. Harris <dharris@harristeq.local>
2017-04-01 06:34:01 -0500

    started a 2way binding

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,5 +1,13 @@
+import {Observable} from "data/observable";
+
+exports.loaded = function(args){
+    var page = args.object;
+    page.bindingContext = new Observable({
+        name:"Dtown"
+    });
+};
 export function onTap(args){
     var button = args.object;
     button.text = "I was tapped";
     console.log("i was tapped");
-}
+};
