6ac6695e74f6f6534296ef84b2e5890efb8bed27
Dominique Harris <revnique@gmail.com>
2017-04-08 20:10:35 -0500

    added ios icons

--- platforms/ios/rpsconf/app/app.ts
+++ platforms/ios/rpsconf/app/app.ts
@@ -8,6 +8,7 @@ import application = require("application");
 application.cssFile = "styles/app.css"
 application.start({ moduleName: "pages/session-page/session-page" });
 //application.start({ moduleName: "pages/main-page/main-page" });
+///usr/local/Cellar/android-sdk/24.4.1_1/tools
 
 /*
 Do not place any code after the application has been started as it will not
