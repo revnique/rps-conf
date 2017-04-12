78ecde879d8dca6970bf78f00d941cc96b06ffb2
Dominique Harris <revnique@gmail.com>
2017-04-12 14:22:16 -0500

    ios and android building for rps-conf

--- rps-conf/platforms/ios/build/rpsconf.build/Debug-iphonesimulator/rpsconf.build/Objects-normal/i386/main.d
+++ rps-conf/platforms/ios/build/rpsconf.build/Debug-iphonesimulator/rpsconf.build/Objects-normal/i386/main.d
@@ -1,7 +1,8 @@
-dependencies: /Users/revnique/git/rps-conf/platforms/ios/internal/main.m \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/revnique/git/rps-conf/platforms/ios/internal/TNSDebugging.h
+dependencies: \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/main.m \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/revnique/git/rps-conf/rps-conf/platforms/ios/internal/TNSDebugging.h
