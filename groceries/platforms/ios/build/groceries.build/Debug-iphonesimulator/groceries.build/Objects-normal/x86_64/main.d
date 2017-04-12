64a5eb6d6a671d15a4c0683445dfc8a65688c7e6
Dominique Harris <revnique@gmail.com>
2017-04-12 14:53:29 -0500

    ios and android working for groceries app

---
+++ groceries/platforms/ios/build/groceries.build/Debug-iphonesimulator/groceries.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/main.m \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/revnique/git/rps-conf/groceries/platforms/ios/internal/TNSDebugging.h
