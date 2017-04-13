1f66a752c965f1b75a63573a76abcf0ca689982d
Dominique Harris <revnique@gmail.com>
2017-04-13 11:28:50 -0500

    ng-chapter-2

--- groceries/platforms/ios/groceries/app/app.component.ts
+++ groceries/platforms/ios/groceries/app/app.component.ts
@@ -3,12 +3,16 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <TextField hint="Email Address" keyboardType="email"
-      autocorrect="false" autocapitalizationType="none"></TextField>
-    <TextField hint="Password" secure="true"></TextField>
+    <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+      <TextField hint="Email Address" keyboardType="email"
+        autocorrect="false" autocapitalizationType="none"></TextField>
+      <TextField hint="Password" secure="true"></TextField>
 
-    <Button text="Sign in"></Button>
-    <Button text="Sign up for Groceries"></Button>
-  `
+      <Button text="Sign in" class="submit-button"></Button>
+      <Button text="Sign up for Groceries"></Button>
+    </StackLayout>
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {}
